d0e20305a0add4dbcd1fe8ee0f7c95916e6c13ea
sosaje <sidandhispetllama@hotmail.com>
2019-01-17 21:29:43 +0000

    Finished first test layout, start of scripting
    Initial test layout complete with all buttons surrounding the keyboard.
    Initial script for control has been started.

---
+++ Assets/GameScripts/textControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textControl : MonoBehaviour {
+
+    List<string> question = new List<string> { "Hold The Keyboard Down", "Press Light Blue", "Press Dark Blue", "Press Purple", "Press Pink", "Press Red", "Press Orange", "Press Yellow", "Press Light Green", "Press Dark Green", "Press Grey", "Press Black"};
+
+    public static int randQuestion = -1;
+
+	// Use this for initialization
+	void Start () {
+        //GetComponent<TextMesh>().text = question[0];
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+        if (randQuestion == -1) {
+            randQuestion = Random.Range(0,11);
+        }
+        if (randQuestion > -1){
+            GetComponent<TextMesh>().text = question[randQuestion];
+        }
+
+
+
+        Debug.Log (question [randQuestion]);
+
+	}
+}
