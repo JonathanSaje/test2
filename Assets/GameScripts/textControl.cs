62cd003b0085c9dbe7a4c0aaf9218074e9726d4f
sosaje <sidandhispetllama@hotmail.com>
2019-02-12 23:57:52 +0000

    All tests Converted, added button guarantee
    All tests now are the correct size, the buttons are now guaranteed to be
    used a certain amount of time depending on the test

--- Assets/GameScripts/textControl.cs
+++ Assets/GameScripts/textControl.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.IO;
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,6 +11,10 @@ public class textControl : MonoBehaviour {
     List<string> question = new List<string>() { "Hold The Keyboard Down", "Press Light Blue", "Press Dark Blue", "Press Purple", "Press Pink", "Press Red", "Press Orange", "Press Yellow", "Press Light Green", "Press Dark Green", "Press Grey", "Press Black", "Task Complete! Please take off your Headset"};
 
     List<string> correctAnswer = new List<string>() {"Kb", "LB", "DB", "Pu", "Pi", "R", "O", "Y", "LG", "DG", "G", "Bl" };
+
+    List<string> correctSubList = new List<string>();
+
+    List<int> availableQuestions = new List<int>();
 
     public static string selectedAnswer;
 
@@ -34,6 +40,8 @@ public class textControl : MonoBehaviour {
 
     public static string localSpaceCollisionPoint;
 
+    public Dictionary<int, int> useChecker = new Dictionary<int, int>();
+
 
 	// Use this for initialization
     // creates the file, sets the time limit if it is the trial test
@@ -55,6 +63,8 @@ public class textControl : MonoBehaviour {
         if (sceneName == "test0") {
             timeLeft = 30.0f;
         }
+        buildUseChecker();
+        buildAvailableQuestions();
 
 	}
 
@@ -62,6 +72,7 @@ public class textControl : MonoBehaviour {
 	void Update () {
 
         timeLeft -= Time.deltaTime;
+
 
         // Sorts between test finished, hold kb and which questions to ask
         if (timeLeft < 0) {
@@ -71,19 +82,26 @@ public class textControl : MonoBehaviour {
             if (randQuestion == 0) {
                 GetComponent<TextMesh>().text = question[randQuestion];
             }
+            System.Random rnd = new System.Random();
 
             if (randQuestion == -1) {
                 if (sceneName == "test0") {
-                    randQuestion = Random.Range(1,4);
+                    randQuestion = UnityEngine.Random.Range(1,4);
                 }
                 if (sceneName == "test1" || sceneName == "test4") {
-                    randQuestion = Random.Range(1,12);
+                    int availableQIndex = rnd.Next(availableQuestions.Count);
+                    randQuestion = availableQuestions[availableQIndex];
+
                 }
                 if (sceneName == "test2") {
-                    randQuestion = Random.Range(1,5);
+                    int availableQIndex = rnd.Next(availableQuestions.Count);
+                    randQuestion = availableQuestions[availableQIndex];
+
                 }
                 if (sceneName == "test3") {
-                    randQuestion = Random.Range(1,8);
+                    int availableQIndex = rnd.Next(availableQuestions.Count);
+                    randQuestion = availableQuestions[availableQIndex];
+
                 }
 
             }
@@ -103,7 +121,7 @@ public class textControl : MonoBehaviour {
                 string contactCoords = contact.point.ToString("F3");
                 contactCoords = contactCoords.Substring(1, contactCoords.Length-2);
 
-                // localSpaceCollisionPoint = localSpaceCollisionPoint.Substring(1, localSpaceCollisionPoint.Length-2);
+
                 buttonSize = buttonSize.Substring(1, buttonSize.Length-2);
 
                 if (correctAnswer[randQuestion]==selectedAnswer) {
@@ -111,14 +129,32 @@ public class textControl : MonoBehaviour {
                     eval = testTime+","+sceneName+","+"y,"+timeTaken+","+selectedAnswer+","+correctAnswer[randQuestion]+","+contact.otherCollider.name+","+contactCoords+","+buttonSize+","+localSpaceCollisionPoint;
                     Save();
                     // Debug.Log(hit.textureCoord);
+
+                    // find index of selectedAnswer, then decrement the value --
+                    int indexOfSelected = correctAnswer.FindIndex(s => s.Equals(selectedAnswer));
+                    // Debug.Log(indexOfSelected);
+                    if (useChecker[indexOfSelected] != 0) {
+                        useChecker[indexOfSelected] = useChecker[indexOfSelected] - 1;
+                    }
+                    buildAvailableQuestions();
                     timeTaken = 0;
                     textControl.randQuestion = 0;
+
                 }else{
                     eval = testTime+","+sceneName+","+"n,"+timeTaken+","+selectedAnswer+","+correctAnswer[randQuestion]+","+contact.otherCollider.name+","+contactCoords+","+buttonSize+","+localSpaceCollisionPoint;
                     Save();
+
+                    int indexOfSelected = correctAnswer.FindIndex(s => s.Equals(selectedAnswer));
+                    // Debug.Log(indexOfSelected);
+                    if (useChecker[indexOfSelected] != 0) {
+                        useChecker[indexOfSelected] = useChecker[indexOfSelected] - 1;
+                    }
+                    // Debug.Log(indexOfSelected);
+                    buildAvailableQuestions();
                     timeTaken = 0;
                     // Debug.Log(hit.textureCoord);
                     textControl.randQuestion = 0;
+
                 }
 
             }
@@ -126,6 +162,69 @@ public class textControl : MonoBehaviour {
         }
 
 	}
+
+    // Builds the useChecker Dictionary
+    void buildUseChecker() {
+
+        if (sceneName == "test1" || sceneName == "test4"){
+            correctSubList = correctAnswer.GetRange(1, 11);
+
+            for (int i = 1; i < correctSubList.Count+1; i++ ) {
+                useChecker.Add(i,2);
+            }
+        }
+        else if (sceneName == "test2"){
+            correctSubList = correctAnswer.GetRange(1, 4);
+            for (int i = 1; i < correctSubList.Count+1; i++ ) {
+                useChecker.Add(i,5);
+            }
+        }
+        else if (sceneName == "test3"){
+            correctSubList = correctAnswer.GetRange(1, 7);
+            for (int i = 1; i < correctSubList.Count+1; i++ ) {
+                useChecker.Add(i,4);
+
+            }
+        }
+
+    }
+
+    // Builds the availableQuestions list by using useChecker
+    void buildAvailableQuestions() {
+        int totalQuestions = correctSubList.Count;
+        int Ototal = 0;
+        availableQuestions.Clear();
+        foreach (KeyValuePair<int,int> i in useChecker) {
+            if (i.Value != 0) {
+                availableQuestions.Add(i.Key);
+            }
+            else {
+                Ototal++;
+            }
+
+            // Debug.Log(i.Key);
+            // Debug.Log(i.Value);
+
+        }
+        Debug.Log(totalQuestions);
+        Debug.Log(Ototal);
+
+        // foreach (int q in availableQuestions) {
+        //     Debug.Log(q);
+        // }
+
+
+        if (Ototal == totalQuestions) {
+            Debug.Log("Rebuild!");
+            useChecker.Clear();
+            buildUseChecker();
+            buildAvailableQuestions();
+
+
+        }
+    }
+
+
 
     // appends the file
     void Save(){
@@ -155,6 +254,8 @@ public class textControl : MonoBehaviour {
 
         return Application.dataPath + "/output/allData.csv";
     }
+
+
 
 
 }
